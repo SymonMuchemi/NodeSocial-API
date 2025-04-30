d070536d0af38ea738ea5a10c85a90f74fc1adbd
symonmuchemi <simonkenya846@gmail.com>
2025-05-01 02:43:28 +0300

    test: add integration tests for user registration and authentication
    This commit introduces a comprehensive suite of integration tests for the authentication controller. It includes tests for user registration, ensuring successful registration and validation of input values. Additionally, it covers user login scenarios, including successful logins, handling incorrect passwords and emails, and verifying consistent error messages for invalid credentials.

---
+++ tests/integration/routes/auth.test.js
@@ -0,0 +1,97 @@
+const request = require('supertest');
+const app = require('../../../src/app.js');
+
+const AUTH_ROUTE = '/api/v1/auth';
+const REG_ROUTE = AUTH_ROUTE + '/register';
+const LOGIN_ROUTE = AUTH_ROUTE + '/login';
+
+const userData = {
+  username: 'JohnDoe',
+  name: 'John Doe',
+  email: 'john.doe@foo.mail',
+  password: '123456',
+};
+
+describe('Auth controller - register user', () => {
+  test('should register a new user', async () => {
+    const res = await request(app).post(REG_ROUTE).send(userData);
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body.success).toBeTruthy();
+    expect(res.body.token).toBeDefined();
+
+    const cookies = res.headers['set-cookie'];
+
+    expect(cookies).toBeDefined();
+    expect(cookies.some((cookie) => cookie.startsWith('token='))).toBeTruthy();
+  });
+
+  test('should not create a new user without proper values', async () => {
+    const res = await request(app).post(REG_ROUTE).send({});
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body.success).toBeFalsy();
+    expect(res.body.error).toBeDefined();
+    expect(res.body.error).toMatch(/Please add/);
+  });
+});
+
+describe('Auth controller - user authentication', () => {
+  test('should log user in sucessfully', async () => {
+    // register user
+    await request(app).post(REG_ROUTE).send(userData);
+
+    const res = await request(app).post(LOGIN_ROUTE).send({
+      email: userData.email,
+      password: userData.password,
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.success).toBeTruthy();
+    expect(res.body.token).toBeDefined();
+
+    const cookies = res.headers['set-cookie'];
+
+    expect(cookies).toBeDefined();
+    expect(cookies.some((cookie) => cookie.startsWith('token='))).toBeTruthy();
+  });
+
+  test('should not log in user with wrong password', async () => {
+    const res = await request(app).post(LOGIN_ROUTE).send({
+      email: userData.email,
+      password: 'something totally wrong',
+    });
+
+    expect(res.statusCode).toBe(401);
+    expect(res.body.success).toBeFalsy();
+    expect(res.body.token).not.toBeDefined();
+  });
+
+  test('should not log in user with wrong email', async () => {
+    const res = await request(app).post(LOGIN_ROUTE).send({
+      email: 'foo@gmail.com',
+      password: userData.password,
+    });
+
+    expect(res.statusCode).toBe(401);
+    expect(res.body.success).toBeFalsy();
+    expect(res.body.token).toBeUndefined();
+    expect(res.body.error).toMatch(/Invalid credentials/)
+  });
+
+  test('should return the same error message when password or email is incorrect', async () => {
+    const wrongPasswordRes = await request(app).post(LOGIN_ROUTE).send({
+      email: userData.email,
+      password: 'a wrong passcode',
+    });
+
+    const wrongEmailRes = await request(app).post(LOGIN_ROUTE).send({
+      email: 'wrong@mail.foo',
+      password: userData.password,
+    });
+
+    expect(wrongEmailRes.statusCode).toEqual(wrongPasswordRes.statusCode);
+    expect(wrongEmailRes.body.success).toEqual(wrongPasswordRes.body.success);
+    expect(wrongEmailRes.body.error).toEqual(wrongPasswordRes.body.error);
+  });
+});
