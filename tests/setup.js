309812338bead56e0d149c6e0a12eb708d00ef68
symonmuchemi <simonkenya846@gmail.com>
2025-04-30 22:59:25 +0300

    feat: implement MongoDB in-memory server setup for tests
    Added setup for MongoDB in-memory server using mongodb-memory-server to facilitate testing. This includes initializing the server before all tests, connecting to the database, and ensuring the database is cleaned up after each test. This change enhances the testing environment by providing a fresh database state for each test run.

--- tests/setup.js
+++ tests/setup.js
@@ -1,2 +1,28 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
 process.env.NODE_ENV = 'test';
 
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+
+  const uri = mongoServer.getUri();
+
+  await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connections.close();
+  await mongoose.stop();
+});
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+});
