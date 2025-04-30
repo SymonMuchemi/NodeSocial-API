4044758cbc402d78bedde50b4d02fb0cb54c5880
symonmuchemi <simonkenya846@gmail.com>
2025-05-01 01:09:31 +0300

    fix: conditionally connect to the database based on environment
    Updated the database connection logic in app.js and db.js to ensure that the connectDB function is only called when the NODE_ENV is not set to 'test'. This prevents the application from attempting to connect to the database during testing, which can lead to unwanted side effects and improve test isolation.

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,12 @@ const mongoose = require('mongoose');
 const { getSecret } = require('../utils/getSecrets.js');
 
 exports.connectDB = async () => {
+  if (process.env.NODE_ENV === 'test') return;
+
   try {
     const mongoURL = await getSecret('MONGO_URL');
     const conn = await mongoose.connect(mongoURL);
+
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
