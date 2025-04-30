6a7251b226be848dc611e39f1200ce911c954a1b
symonmuchemi <simonkenya846@gmail.com>
2025-04-30 12:24:35 +0300

    feat: add cookie-parser middleware for cookie handling
    This commit introduces the cookie-parser middleware to the application. The cookie-parser is now included in the package.json dependencies and is utilized in the index.js file to parse cookies attached to the client request. This enhancement will facilitate cookie management and improve the handling of user sessions and authentication.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 
 const { connectDB } = require('./config/db.js');
 const errorHandler = require('./middleware/error.js');
@@ -22,6 +23,7 @@ if (NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
